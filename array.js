e3ab64b45e819e359c1361ac34e4107f58e6aa19
shurupxgvozd <shurupxgvozd@gmail.com>
2025-03-25 21:48:27 +0000

    compito per casa di array

--- array.js
+++ array.js
@@ -1,14 +1,32 @@
 let colori = ["red", "green", "orange", "Aquamarine", "Silver", "violet", "gold"];
+let contenitore = document.getElementById("contenitore");
+ 
 
-let contenitor = document.getElementById("contenitore");
-for (let index = 0; index < colori.length; index++){
-const elemento = colori[index];
-const coloretesto = colori[colori.length-1 - index]
-console.log(elemento);
+let style = document.createElement('style');
+document.head.appendChild(style);
+let stylesheet = style.sheet;
+ 
+for (let index = 0; index < colori.length; index++) {
+  const elemento = colori[index];
+  const coloreTesto = colori[colori.length - 1 - index];
+  const fontSize = (index + 1) * 10 + "px";
+ 
+  const nuovoParagrafo = document.createElement("p");
+  nuovoParagrafo.textContent = elemento;
+ 
+ 
+  let className = "paragrafo-" + index;
+  nuovoParagrafo.classList.add(className);
+ 
+  contenitore.appendChild(nuovoParagrafo);
+ 
 
-const nuovoParagrafo = document.createElement("p");
-nuovoParagrafo.textContent = elemento;
-nuovoParagrafo.style.backgroundColor = elemento;
-nuovoParagrafo.style.color = coloretesto;
-contenitor.appendChild(nuovoParagrafo);
+  stylesheet.insertRule(`.${className} {
+    background-color: ${elemento};
+    color: ${coloreTesto};
+    font-size: ${fontSize};
+    margin-right: ${index}%;
+    padding: ${index}%;
+    border: 2px solid black;
+  }`, stylesheet.cssRules.length);
 }
