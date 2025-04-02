53a373b7ef359a914fc1cb8564b0be4533abe55e
shurupxgvozd <shurupxgvozd@gmail.com>
2025-04-02 06:26:03 +0000

    array and functions

--- array.js
+++ array.js
@@ -1,32 +1,89 @@
-let colori = ["red", "green", "orange", "Aquamarine", "Silver", "violet", "gold"];
+let colori = ["red","black","white","green","yellow","pink"];
+
+let arrayImmagini = ["cammello.jpeg","giraffa.jpeg","gufo.jpeg","falco.jpeg","cammello.jpeg","giraffa.jpeg","gufo.jpeg","falco.jpeg"];
+
 let contenitore = document.getElementById("contenitore");
- 
+let vociMenu = ["Home","About","Contatti","Shop"]
 
-let style = document.createElement('style');
-document.head.appendChild(style);
-let stylesheet = style.sheet;
- 
+let barraNavigazione = document.createElement("nav");
+barraNavigazione.id = "nav";
+barraNavigazione.style.backgroundColor = "aqua";
+barraNavigazione.style.padding = "2%";
+contenitore.appendChild(barraNavigazione);
+
+for (let index = 0; index < vociMenu.length; index++) {
+    const nuovaVoce = document.createElement("a");
+    nuovaVoce.textContent = vociMenu[index];
+    nuovaVoce.style.margin = "2%";
+    nuovaVoce.style.backgroundColor = "grey";
+    nuovaVoce.style.padding = "1%";
+    
+
+    barraNavigazione.appendChild(nuovaVoce);
+
+}
+
+
+
 for (let index = 0; index < colori.length; index++) {
-  const elemento = colori[index];
-  const coloreTesto = colori[colori.length - 1 - index];
-  const fontSize = (index + 1) * 10 + "px";
- 
-  const nuovoParagrafo = document.createElement("p");
-  nuovoParagrafo.textContent = elemento;
- 
- 
-  let className = "paragrafo-" + index;
-  nuovoParagrafo.classList.add(className);
- 
-  contenitore.appendChild(nuovoParagrafo);
- 
+    const elemento = colori[index];
+    const coloreTesto = colori[colori.length-1 - index]
+    console.log(elemento);
 
-  stylesheet.insertRule(`.${className} {
-    background-color: ${elemento};
-    color: ${coloreTesto};
-    font-size: ${fontSize};
-    margin-right: ${index}%;
-    padding: ${index}%;
-    border: 2px solid black;
-  }`, stylesheet.cssRules.length);
+    const nuovoParagrafo = document.createElement("p");
+    nuovoParagrafo.textContent = elemento;
+    nuovoParagrafo.style.backgroundColor = elemento;
+    nuovoParagrafo.style.color = coloreTesto;
+    nuovoParagrafo.style.textAlign = "center";
+    nuovoParagrafo.style.marginRight = index * 5 + "%";
+    nuovoParagrafo.style.marginLeft = index * 5 + "%";
+    nuovoParagrafo.style.fontSize = (index+1)*10  + "px";
+    nuovoParagrafo.style.padding = index + "%";
+
+
+    contenitore.appendChild(nuovoParagrafo);
+}
+
+
+
+for (let index = 0; index < arrayImmagini.length; index++) {
+    const nuovaImmagine = document.createElement("img");
+    nuovaImmagine.src = arrayImmagini[index];
+    nuovaImmagine.alt = arrayImmagini[index];
+    nuovaImmagine.style.height = "200px";
+    nuovaImmagine.style.width = "300px";
+    nuovaImmagine.style.margin = "1%";
+    contenitore.appendChild(nuovaImmagine);
+}
+
+
+for (let index = 0; index < colori.length; index++) {
+    const elemento = colori[index];
+    const coloreTesto = colori[colori.length-1 - index]
+    console.log(elemento);
+
+    const nuovoParagrafo = document.createElement("p");
+    nuovoParagrafo.textContent = elemento;
+    nuovoParagrafo.style.backgroundColor = elemento;
+    nuovoParagrafo.style.color = coloreTesto;
+    nuovoParagrafo.style.textAlign = "center";
+    nuovoParagrafo.style.marginRight = index * 5 + "%";
+    nuovoParagrafo.style.marginLeft = index * 5 + "%";
+    nuovoParagrafo.style.fontSize = (index+1)*10  + "px";
+    nuovoParagrafo.style.padding = index + "%";
+
+
+    contenitore.appendChild(nuovoParagrafo);
+}
+
+
+
+for (let index = 0; index < arrayImmagini.length; index++) {
+    const nuovaImmagine = document.createElement("img");
+    nuovaImmagine.src = arrayImmagini[index];
+    nuovaImmagine.alt = arrayImmagini[index];
+    nuovaImmagine.style.height = "200px";
+    nuovaImmagine.style.width = "300px";
+    nuovaImmagine.style.margin = "1%";
+    contenitore.appendChild(nuovaImmagine);
 }
---
+++ functions.js
@@ -0,0 +1,12 @@
+
+
+function cambiaSfondoInVerde(){
+    const elementoContenitore = document.getElementById("contenitore");
+    elementoContenitore.style.backgroundColor = "green";
+}
+
+function cambiaMargine(){
+    const elementoContenitore = document.getElementById("contenitore");
+    elementoContenitore.style.margin = "5%";
+
+}
